06a1fe2d72cf8aaf4724408ae122e62362fe48b3
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-25 15:07:46 +0530

    04-landing-page

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "@/features/home/components/header";
+import { Footer } from "@/features/home/components/footer";
+
+export default async function HomeLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <Header />
+            <main className="z-20 relative w-full pt-0 md:pt-0">
+                {children}
+            </main>
+            <Footer />
+        </>
+    );
+}
