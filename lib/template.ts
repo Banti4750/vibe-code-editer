ba2581d3c895630a18709c77b4b2ea0a7d61d6e3
Banti4750 <bantikumar6203818460@gmail.com>
2025-08-31 17:18:11 +0530

    file-explorer-part-2

--- lib/template.ts
+++ lib/template.ts
@@ -1,10 +1,9 @@
 // templatePaths.ts
 export const templatePaths = {
     REACT: '/starters-main/react-ts',
-    NEXTJS: '/starters-main/nextjs-new',
+    NEXTJS: '/starters-main/nextjs',
     EXPRESS: '/starters-main/express-simple',
     VUE: '/starters-main/vue',
     HONO: '/starters-main/hono-nodejs-starter',
     ANGULAR: '/starters-main/angular',
 };
-// X:\Mern-project_100\vibe-code-editer\starters-main
